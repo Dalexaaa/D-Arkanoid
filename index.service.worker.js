c0f5604a4fc771bf684f4a48a61e525fbb8c2f1d
Dalexaaa <Dalexaaa@users.noreply.github.com>
2026-07-02 00:18:40 +0000

    deploy: 5c2eb55ed4d85794c9f76da8cc46e392285f52bf

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782951404|3283232';
+const CACHE_VERSION = '1782951507|3292613';
 /** @type {string} */
 const CACHE_PREFIX = 'arkanoid-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
